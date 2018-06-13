a8f97c81af7f5554d9f114079fd8a6361f6ab2dd
jiawenng <ng.jia.wen@hotmail.com>
2018-06-13 16:25:45 +0800

    Added view and template for detailed blogpost

--- First_Django/settings.py
+++ First_Django/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'First_Django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'blog/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path(r'', views.post_list, name='post_list'),
+    re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+
 ]
